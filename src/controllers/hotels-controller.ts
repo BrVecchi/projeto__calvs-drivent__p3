ce1d69aaf562cae4919a0ab2ec1273bd52569c3e
BrVecchi <bno.vecchi@gmail.com>
2023-02-04 19:05:36 -0300

    feat: add hotels controller and routes

---
+++ src/controllers/hotels-controller.ts
@@ -0,0 +1,25 @@
+import { Response } from 'express';
+import httpStatus from 'http-status';
+
+import { AuthenticatedRequest } from '@/middlewares';
+import hotelsService from '@/services/hotels-service';
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  try {
+    const listHotels = await hotelsService.getHotelsServ();
+    return res.status(httpStatus.OK).send(listHotels);
+  } catch (error) {
+    return res.status(httpStatus.NOT_FOUND).send({});
+  }
+}
+
+export async function getHotelById(req: AuthenticatedRequest, res: Response) {
+  const id = Number(req.params.id);
+
+  try {
+    const hotel = await hotelsService.getHotelByIdServ(id);
+    return res.status(httpStatus.OK).send(hotel);
+  } catch (error) { 
+    return res.status(httpStatus.BAD_REQUEST).send({});
+  }
+}
--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { getHotelById, getHotels } from '@/controllers/hotels-controller';
 import { authenticateToken } from '@/middlewares';
+import { handleConfirmedStay } from '@/middlewares/confirmed-stay-middleware';
 
 const hotelsRouter = Router();
 
 hotelsRouter
-  .all("/*", authenticateToken)
+  .all("/*", authenticateToken, handleConfirmedStay)
   .get("/", getHotels)
   .get("/:id", getHotelById);
 
